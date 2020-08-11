3d21bbd2ae93c7a695792fdb037aee2b7c815131
Cadence Ember <cloudrac3r@vivaldi.net>
2020-08-12 02:05:22 +1200

    Add configuration

---
+++ configuration.sample.py
@@ -0,0 +1,6 @@
+# ==============================
+#  You MUST set these settings.
+# ==============================
+
+# A URL that this site can be accessed on. Do not include a trailing slash.
+website_origin = "http://example.com:3000"
