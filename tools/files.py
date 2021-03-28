0f877b06bc3c57064514c3b476f1b8fe52c37fc7
Cadence Ember <cadence@disroot.org>
2021-03-28 23:58:54 +1300

    Fix temporary file removal

--- tools/files.py
+++ tools/files.py
@@ -3,7 +3,9 @@ import os
 def get_created_files(id):
 	if id[0] == "-":
 		id = "_" + id[1:] # youtube-dl changes - to _ at the start, presumably to not accidentally trigger switches with * in shell
-	return (f for f in os.listdir() if f.startswith("{}_".format(id)))
+	id += "_"
+	trim_id = id.lstrip("-_")
+	return (f for f in os.listdir() if f.startswith(id) or f.startswith(trim_id))
 
 def clean_up_temp_files(id):
 	created_files = get_created_files(id)
