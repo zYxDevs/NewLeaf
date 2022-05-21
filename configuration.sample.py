c9b16d3efd0b63390996ef7dc313d313251a7507
Cadence Ember <cadence@disroot.org>
2022-05-22 00:57:13 +1200

    Explain bind_host in sample configuration

--- configuration.sample.py
+++ configuration.sample.py
@@ -11,6 +11,7 @@ website_origin = "http://example.com:3000"
 # ==============================
 
 # The address of the interface to bind to.
+# If you don't know what "bind addresses" are, the default of "0.0.0.0" is probably good. If you only want NewLeaf to be accessible on this same machine, not from any other networks, then you can use "127.0.0.1". If CloudTube never updates subscriptions, even after you wait for some time, try using "::1".
 #bind_host = "0.0.0.0"
 
 # The port to bind to.
